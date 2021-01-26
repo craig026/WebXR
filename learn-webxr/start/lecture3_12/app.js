9f32aa179ea8223b825b3d99ba7cb15c97e11562
craig026 <54131430+craig026@users.noreply.github.com>
2021-01-26 08:59:14 +0800

    Updated Lecture 3_12

--- learn-webxr/start/lecture3_12/app.js
+++ learn-webxr/start/lecture3_12/app.js
@@ -122,7 +122,7 @@ class App{
         this.loadKnight();
 
         this.reticle = new THREE.Mesh(
-            new THREE.RingBufferGeometry(0.15, 0.2, 32).rotateX(-Math.PI/2);
+            new THREE.RingBufferGeometry(0.15, 0.2, 32).rotateX(-Math.PI/2),
             new THREE.MeshBasicMaterial();
             this.reticle.visible = false;
             this.scene.add(this.reticle);
